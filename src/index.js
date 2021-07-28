6f41233639b9cb39b1dd8c4d494870256233c740
nirvan1234 <nirvanpandagre@gmail.com>
2021-07-28 16:56:29 +0530

    change in index file

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const hbs = require('hbs');
-const port =  8000;
+const port = process.env.PORT || 8000;
 
 
 const static_path = path.join(__dirname , "../public");
